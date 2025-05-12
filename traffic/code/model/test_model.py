29c8231b936d6e5fa559111dea6f0fdfcf7ec4cf
pmlam27 <p.m.benlam@gmail.com>
2025-05-12 20:51:52 +0800

    training with log

--- traffic/code/model/test_model.py
+++ traffic/code/model/test_model.py
@@ -5,7 +5,7 @@ import traci
 # Load the trained model
 model = PPO.load("output/traffic_light_optimizer_mk2.zip")
 
-env = TrafficLightEnv(False)
+env = TrafficLightEnv(True)
 
 obs, _ = env.reset()
 
@@ -25,7 +25,7 @@ while not done:
     reward_total += reward
 
     times.append(env.get_time())
-    vehicle_counts.append(obs.sum())
+    vehicle_counts.append(obs.tolist()[0] + obs.tolist()[1] + obs.tolist()[2])
     
     done = terminated or truncated
 
@@ -33,27 +33,27 @@ env.close()
 print(reward_total)
 reward_total = 0
 
-# sumo_cmd = ['sumo', '-c', '../../config/traffic.sumocfg']
-# traci.start(sumo_cmd)
+sumo_cmd = ['sumo', '-c', '../../config/traffic.sumocfg']
+traci.start(sumo_cmd)
 
-# hourly_sum = 0
+hourly_sum = 0
 
-# for i in range(24100):
-#     if (i % 300 == 0):
-#         vehicle_counts2.append(hourly_sum)
-#         hourly_sum = 0
-#     else:
-#         hourly_sum += traci.lane.getLastStepVehicleNumber("east_to_center_0")
-#         hourly_sum += traci.lane.getLastStepVehicleNumber("south_to_center_0")
-#         hourly_sum += traci.lane.getLastStepVehicleNumber("west_to_center_0")
-#     traci.simulationStep()
+for i in range(24100):
+    if (i % 300 == 0):
+        vehicle_counts2.append(hourly_sum)
+        hourly_sum = 0
+    else:
+        hourly_sum += traci.lane.getLastStepVehicleNumber("east_to_center_0")
+        hourly_sum += traci.lane.getLastStepVehicleNumber("south_to_center_0")
+        hourly_sum += traci.lane.getLastStepVehicleNumber("west_to_center_0")
+    traci.simulationStep()
 
-# traci.close()
+traci.close()
 
 # Plot the result
 plt.figure(figsize=(10, 5))
 plt.plot(times, vehicle_counts, marker='o', linestyle='-', color='blue')
-# plt.plot(times, vehicle_counts2, marker='o', linestyle='-', color='red')
+plt.plot(times, vehicle_counts2, marker='o', linestyle='-', color='red')
 plt.title("Number of Vehicles on the Road vs. Time")
 plt.xlabel("Simulation Step (time)")
 plt.ylabel("Number of Vehicles")
--- traffic/code/model/train.py
+++ traffic/code/model/train.py
@@ -1,17 +1,27 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from env import TrafficLightEnv
+from stable_baselines3.common.monitor import Monitor
+from datetime import datetime
 
 print("Checking environment...")
 # Create environment
 env = TrafficLightEnv(False)
 # check_env(env)  # Verify environment follows Gym interface
 
+now = datetime.now()
+
+# Format: YYYYMMDD_HHMMSS
+timestamp = now.strftime("%Y%m%d_%H%M%S")
+
+env = Monitor(env, filename=f"log/monitor_mk3_{timestamp}")
+
 print("Environment check complete.")
 # Initialize RL model
 # model = PPO("MlpPolicy", env, verbose=1)
-# model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.0003, n_steps=1024, batch_size=64, n_epochs=10)
-model = PPO.load("output/traffic_light_optimizer_mk2.zip", env=env)
+# model = PPO("MlpPolicy", env, verbose=1, learning_rate=0.0003, n_steps=2048, batch_size=64, n_epochs=10)
+model = PPO.load("output/traffic_light_optimizer_mk3.zip", env=env)
+model.n_steps = 1024
 # model.env = env
 
 # Train the model
@@ -21,6 +31,6 @@ model.learn(total_timesteps=10000)
 # model.learn(total_timesteps=10000)
 
 # Save the model
-model.save("output/traffic_light_optimizer_mk2")
+model.save("output/traffic_light_optimizer_mk3")
 
 env.close()
